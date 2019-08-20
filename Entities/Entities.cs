0d07d4df511cf5eedf18d2c36177a49d7a1f863b
Bruno Gomes <brunoo.gomes11@gmail.com>
2019-08-20 16:26:45 -0300

    Exercicio usando Linq Versão Final!

--- Entities/Entities.cs
+++ Entities/Entities.cs
@@ -1,7 +1,15 @@
 namespace ExercicioUdemy2.Entities
 {
-    public class Entities
+    public class Employee
     {
-        
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary){
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using ExercicioUdemy2.Entities;
+using System.Collections.Generic;
+
 
 namespace ExercicioUdemy2
 {
@@ -6,7 +12,44 @@ namespace ExercicioUdemy2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> emp = new List<Employee>();
+
+            Console.Write("Enter salary: ");
+            double salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        try{
+            using (StreamReader sr = File.OpenText(path)){
+                
+                while(!sr.EndOfStream){
+                string[] vet = sr.ReadLine().Split(',');
+                string name = vet[0];
+                string email = vet[1];
+                double sal = double.Parse(vet[2],CultureInfo.InvariantCulture);
+
+                emp.Add(new Employee(name,email,sal));
+                }
+                
+            }
+            Console.WriteLine("Email of people whose salary is more than 2000.00: ");
+            
+            var emaill = emp.Where(e => e.Salary > salary).OrderBy(e => e.Email).Select(e => e.Email);
+
+            foreach(string emails in emaill){
+                Console.WriteLine(emails);
+            }
+            
+            var soma = emp.Where(s => s.Name[0] =='M').Sum(s => s.Salary);
+
+                Console.WriteLine("Sum of salary of people whose name starts with 'M': " + soma.ToString("F2",CultureInfo.InvariantCulture));
+
+            }
+            catch(IOException e){
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
